f242a3f33df13c51e95c19d40e0d9675bc61efad
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-08-27 17:51:28 +0300

    27082025

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const choiceOfWorkplace = document.getElementsByClassName("choiceOfWorkplace");
+const workPlace = document.getElementsByClassName("workPlace");
+const allTd = document.querySelectorAll("td");
+
+for (let index = 0; index < choiceOfWorkplace.length; index++) {
+    choiceOfWorkplace[index].addEventListener("click", (event) => {        
+        for (let index = 0; index < allTd.length; index++) {
+            allTd[index].style = "border: 1px solid var(--basicColor)";            
+        }        
+        choiceOfWorkplace[index].style.borderBottomColor = "var(--basicBG)";
+    });    
+}
+
+
+const day = new Date().getDate();
+const month = new Date().getMonth() + 1;
+const year = new Date().getFullYear();
+
+const lastDayOfMonth = new Date(year, month + 1, 0);
+let date = new Date(year, month, 1);
+ 
+for (let index = 0; index < 10; index++) {
+    console.log(date.setDate(date.getDate() + index), month, year);
+    
+}
+
+// console.log(day, month, year);
